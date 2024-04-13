4c61a0d0a4b8e97e9ad0eed41a8d104c6625562a
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-04-13 17:50:50 +0530

    Loan Component created

---
+++ frontend/src/components/LoanComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoanComponent = () => {
+  return (
+    <div className="border-2 border-primaryGray rounded-lg p-4 w-96 h-72">
+      Loan
+    </div>
+  );
+};
+
+export default LoanComponent;
--- frontend/src/components/Loans.jsx
+++ frontend/src/components/Loans.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import LoanComponent from "./LoanComponent";
 
 const Loans = () => {
-  return <div>Loans</div>;
+  return (
+    <section className="h-full overflow-y-auto no-scrollbar">
+      <h1 className="text-primaryYellow text-4xl font-medium mb-6">Loans</h1>
+      <div className="gap-5 flex flex-wrap">
+        <LoanComponent />
+        <LoanComponent />
+        <LoanComponent />
+        <LoanComponent />
+      </div>
+    </section>
+  );
 };
 
 export default Loans;
