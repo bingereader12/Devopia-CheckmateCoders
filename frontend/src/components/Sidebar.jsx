14aaecb133dc6301a4b58f0d9abfeee07b362c6c
Aryan <aryanshirsat357@gmail.com>
2024-04-13 11:42:38 +0530

    sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,11 +1,26 @@
-import React from "react";
-
+import {React,useState} from 'react'
+import { NavLink } from 'react-router-dom';
+// import { RiHome5Fill,RiHome5Line } from "react-icons/ri";
+// import { IoDocuments,IoDocumentsOutline,IoSettings,IoSettingsOutline } from "react-icons/io5";
+// import { MdExplore,MdOutlineExplore  } from "react-icons/md";
 const Sidebar = () => {
-  return (
-    <div className="border border-primaryGray flex flex-col w-80 h-full">
-      Sidebar
-    </div>
-  );
-};
+  const [activeLink, setActiveLink] = useState(0);
 
-export default Sidebar;
+  const handleLinkClick = (index) => {
+    setActiveLink(index);
+  };
+  return (
+    <div className='flex flex-col w-full h-full gap-10 place-items-center'>
+      <h3 className='text-xl font-semibold'>Internship Portal</h3>
+      <div className='flex flex-col gap-6 text-justify font-semibold'>
+        <NavLink to="/dashboard" className={`link ${activeLink ? 'active' : ''}`} onClick={() => handleLinkClick(0)}>
+          <button className='flex flex-row gap-2'>
+          {activeLink ? <RiHome5Fill style={{fontSize: 22}}/> : <RiHome5Line style={{fontSize: 22}}/>}Dashboard
+          </button>
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
