294fe8380344abf0c654909b70762ea00d48cacd
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2024-04-13 11:12:01 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Routes></Routes>
+    </div>
+  );
+};
+
+export default App;
