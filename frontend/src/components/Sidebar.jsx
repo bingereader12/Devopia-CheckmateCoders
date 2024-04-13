0ede56ac5ed9d17e53f7d309ab353f82ef909432
jsbhumra <jagjit.s.bhumra@gmail.com>
2024-04-13 20:59:42 +0530

    Logout added to sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import savingsIcon from "../../public/icons/wallet.svg";
 import transactionsIcon from "../../public/icons/card.svg";
+import logoutIcon from "../../public/icons/log-out.svg";
 import investmentsIcon from "../../public/icons/bar-chart.svg";
 import loansIcon from "../../public/icons/wallet.svg";
 import insuranceIcon from "../../public/icons/folder.svg";
@@ -22,24 +23,33 @@ const Sidebar = ({ handleClick }) => {
   return (
     <div className="w-96 pl-8 h-full flex flex-col gap-10 text-left p-2 cursor-pointer bg-[#161616] ronuded-lg">
       <h1 className="text-3xl font-bold text-primaryYellow ml-2 w-full mt-8">Finance App</h1>
-      <div className="flex flex-col gap-5">
-        {menu.map((item, index) => {
-          const isActive = item.name === activeTab;
-          return (
-            <span
-              key={index}
-              onClick={() => handleTabClick(item)}
-              className={`p-2 text-lg flex ${
-                isActive
-                  ? "bg-[#fffff] rounded-full text-white"
-                  : "text-gray-400"
-              }`}
-            >
-              <img src={item.icon} alt="icon" className="h-5 w-5 mr-3 mt-1" />
-              {item.name}
-            </span>
-          );
-        })}
+      <div className="flex flex-col h-full justify-between my-2">
+        <div className="flex flex-col gap-5">
+          {menu.map((item, index) => {
+            const isActive = item.name === activeTab;
+            return (
+              <span
+                key={index}
+                onClick={() => handleTabClick(item)}
+                className={`p-2 text-lg flex ${
+                  isActive
+                    ? "bg-[#fffff] rounded-full text-white"
+                    : "text-gray-400"
+                }`}
+              >
+                <img src={item.icon} alt="icon" className="h-5 w-5 mr-3 mt-1" />
+                {item.name}
+              </span>
+            );
+          })}
+        </div>
+        <div
+          onClick={() => {}}
+          className={`p-2 text-xl font-bold flex mb-3`}
+        >
+          <img src={logoutIcon} alt="icon" className="h-6 w-6 mr-3 mt-1" />
+          <span className="text-[#ff4444]">Logout</span>
+        </div>
       </div>
     </div>
   );
